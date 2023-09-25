9b842f25b31be2ee52854363ea7e19f5cbd317b5
anika235 <anikatabassum.2662@gmail.com>
2023-09-25 23:06:10 +0600

    idk what this is

---
+++ app/src/main/java/com/example/auracle/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.auracle
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+        val nxtbutton : Button = view.findViewById(R.id.explorebutton)
+        nxtbutton.setOnClickListener {
+            val fragment = exploreFragment()
+
+        }
+
+        return v
+    }
+
+}
--- app/src/main/java/com/example/auracle/Homepage.kt
+++ app/src/main/java/com/example/auracle/Homepage.kt
@@ -1,19 +1,47 @@
 package com.example.auracle
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.inputmethod.InputBinding
+import androidx.fragment.app.Fragment
+import com.example.auracle.databinding.ActivityHomepageBinding
 import com.example.auracle.firebase.Authenticate
 
 class Homepage : AppCompatActivity() {
 
     private val TAG = "HomePage"
+    private lateinit var binding : ActivityHomepageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        binding = ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replacefragment(HomeFragment())
 
+        binding.bottomNavigationView.setOnItemReselectedListener {
+
+            when(it.itemId){
+                R.id.home_fragment -> replacefragment(HomeFragment())
+                R.id.explore_fragment -> replacefragment(exploreFragment())
+                R.id.library_fragment -> replacefragment(LibraryFragment())
+
+                else->{
+                }
+            }
+            true
+        }
         Log.d(TAG, Authenticate().user().toString())
-
     }
+
+    private fun replacefragment(fragment: Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
