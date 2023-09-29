37b378830cdd07e99bf6702fa70e88a9983319ec
anika235 <anikatabassum.2662@gmail.com>
2023-09-30 00:15:42 +0600

    Onek Kichu add korsi

--- app/src/main/java/com/example/auracle/LibraryFragment.kt
+++ app/src/main/java/com/example/auracle/LibraryFragment.kt
@@ -1,19 +1,22 @@
 package com.example.auracle
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.auracle.firebase.Authenticate
 import com.example.auracle.podcastcard.PodcastCard
 import com.example.auracle.podcastcard.PodcastCardAdapter
 
 class LibraryFragment : Fragment() {
 
-    private lateinit var rcvPodcasts: RecyclerView
+    private lateinit var signoutButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +28,20 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_library, container, false)
+        val v = inflater.inflate(R.layout.fragment_library, container, false)
 
-        val podcastList = arrayListOf<PodcastCard>()
-        val cardId = arrayOf(
-            R.drawable.p1,
-            R.drawable.p2,
-            R.drawable.p3,
-            R.drawable.p4,
-            R.drawable.p5,
-        )
+        signoutButton = v.findViewById(R.id.signoutbutton)
+        signoutButton.setOnClickListener{
+            Authenticate().signOut()
+            requireActivity().startActivity(Intent(requireActivity(), StartPage::class.java))
+        }
+//        val nxtbutton : Button = view.findViewById(R.id.explorebutton)
+//        nxtbutton.setOnClickListener {
+//            val fragment = exploreFragment()
+//
+//        }
 
-        for (id in cardId) podcastList.add(PodcastCard(id))
-
-        rcvPodcasts = view.findViewById(R.id.rcvPodcastList)
-        rcvPodcasts.layoutManager = GridLayoutManager(context, 3)
-        rcvPodcasts.adapter = PodcastCardAdapter(podcastList)
-        rcvPodcasts.setHasFixedSize(true)
-
-        return view
+        return v
     }
 
 }
--- app/src/main/java/com/example/auracle/exploreFragment.kt
+++ app/src/main/java/com/example/auracle/exploreFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.auracle.podcastcard.PodcastCard
+import com.example.auracle.podcastcard.PodcastCardAdapter
 
 class exploreFragment : Fragment() {
+    private lateinit var rcvPodcasts: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,25 @@ class exploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        val view = inflater.inflate(R.layout.fragment_explore, container, false)
+
+        val podcastList = arrayListOf<PodcastCard>()
+        val cardId = arrayOf(
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p3,
+            R.drawable.p4,
+            R.drawable.p5,
+        )
+
+        for (id in cardId) podcastList.add(PodcastCard(id))
+
+        rcvPodcasts = view.findViewById(R.id.rcvPodcastList)
+        rcvPodcasts.layoutManager = GridLayoutManager(context, 3)
+        rcvPodcasts.adapter = PodcastCardAdapter(podcastList)
+        rcvPodcasts.setHasFixedSize(true)
+
+        return view
     }
 
 }
