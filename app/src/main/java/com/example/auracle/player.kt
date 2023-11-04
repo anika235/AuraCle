5ec7b9631acdad46fe18d7f003e02a9ba945c2a6
anika235 <anikatabassum.2662@gmail.com>
2023-11-04 23:50:57 +0600

    crash kore khali

--- app/src/main/java/com/example/auracle/player.kt
+++ app/src/main/java/com/example/auracle/player.kt
@@ -3,8 +3,10 @@ package com.example.auracle
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.auracle.api.ListenNoteApi
 import com.example.auracle.databinding.ActivityPlayerBinding
 import com.example.auracle.datapack.listennote.ListenEpisodeShort
+import com.squareup.picasso.Picasso
 
 class player : AppCompatActivity() {
 
@@ -12,6 +14,7 @@ class player : AppCompatActivity() {
         lateinit var podcastlistPA: ArrayList<ListenEpisodeShort>
         var podcastPosition: Int = 0
         var mediaPlayer: MediaPlayer? = null
+        var isPlaying:Boolean = false
 
     }
     private lateinit var podcastId: String
@@ -20,24 +23,61 @@ class player : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeLayout()
+        binding.playPauseButton.setOnClickListener{
+            if(isPlaying) pausePodcast()
+            else playPodcast()
 
+        }
+    }
+    private fun setLayout()
+    {
+        val podcastDetails = ListenNoteApi().podcastDetails(podcastId)
+        Picasso.get().load(podcastDetails.thumbnail).into(binding.PodcastThumbnail)
+        binding.PodcastNamePA.text = podcastlistPA[podcastPosition].title
+    }
+    private fun createMediaPlayer()
+    {
+        try {
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+            mediaPlayer!!.reset()
+            val audioPath = podcastlistPA[podcastPosition].audio
+            mediaPlayer!!.setDataSource(audioPath)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPauseButton.setIconResource(R.drawable.pause)
+        }
+        catch (e: Exception) {
+            return
+        }
+    }
+    private fun initializeLayout()
+    {
         podcastPosition = intent.getIntExtra("index", 0)
         when (intent.getStringExtra("class")){
             "EpisodeCardAdapter" ->{
                 podcastlistPA = ArrayList()
                 podcastlistPA.addAll(PodcastDetails.episodes)
-                if(mediaPlayer == null) mediaPlayer = MediaPlayer()
-                mediaPlayer!!.reset()
-                val audioPath = podcastlistPA[podcastPosition].audio
-                mediaPlayer!!.setDataSource(audioPath)
-                mediaPlayer!!.prepare()
-                mediaPlayer!!.start()
-
+                setLayout()
+                createMediaPlayer()
             }
-
         }
+    }
+    private fun playPodcast()
+    {
+        binding.playPauseButton.setIconResource(R.drawable.pause)
+        isPlaying = true
+        mediaPlayer!!.start()
 
     }
 
+    private fun pausePodcast()
+    {
+        binding.playPauseButton.setIconResource(R.drawable.play)
+        isPlaying = false
+        mediaPlayer!!.pause()
+
+    }
 
 }
